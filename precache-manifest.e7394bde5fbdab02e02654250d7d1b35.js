087193075d76fe5f9faf19b70b6ed9d37fc80b9a
Fahrudin Halilovic <fahrudin@occrp.org>
2020-07-12 12:09:45 +0200

    Updates

---
+++ precache-manifest.e7394bde5fbdab02e02654250d7d1b35.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3194ffaef0b523cb3c9ecf50c19cadb4",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "3425610d36a19fb60c9f",
+    "url": "/build/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "4a154cd4fc0bfc540278",
+    "url": "/build/static/css/main.88af7433.chunk.css"
+  },
+  {
+    "revision": "3425610d36a19fb60c9f",
+    "url": "/build/static/js/2.c61dc036.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/build/static/js/2.c61dc036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a154cd4fc0bfc540278",
+    "url": "/build/static/js/main.ae0b8121.chunk.js"
+  },
+  {
+    "revision": "04a039a974d3031683de",
+    "url": "/build/static/js/runtime-main.6f1a77dd.js"
+  },
+  {
+    "revision": "c5109a1c3ff7281895bc99f534c12ebd",
+    "url": "/build/static/media/KuranKerimFontHamdullah.c5109a1c.ttf"
+  }
+]);
