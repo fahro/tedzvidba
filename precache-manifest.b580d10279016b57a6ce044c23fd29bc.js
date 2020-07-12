4a2a1ee94aceba472b727ad2b7e9e995deead708
Fahrudin Halilovic <fahrudin@occrp.org>
2020-07-12 12:30:59 +0200

    Updates

---
+++ precache-manifest.b580d10279016b57a6ce044c23fd29bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248ca1a777c195b181a2bfaf794683fc",
+    "url": "/tedzvidba.github.io/index.html"
+  },
+  {
+    "revision": "3425610d36a19fb60c9f",
+    "url": "/tedzvidba.github.io/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "76b220a0f88141934568",
+    "url": "/tedzvidba.github.io/static/css/main.97699a76.chunk.css"
+  },
+  {
+    "revision": "3425610d36a19fb60c9f",
+    "url": "/tedzvidba.github.io/static/js/2.c61dc036.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tedzvidba.github.io/static/js/2.c61dc036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76b220a0f88141934568",
+    "url": "/tedzvidba.github.io/static/js/main.fa77d577.chunk.js"
+  },
+  {
+    "revision": "2ca700aa0ce3a137f9f4",
+    "url": "/tedzvidba.github.io/static/js/runtime-main.5be61f3b.js"
+  },
+  {
+    "revision": "c5109a1c3ff7281895bc99f534c12ebd",
+    "url": "/tedzvidba.github.io/static/media/KuranKerimFontHamdullah.c5109a1c.ttf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/build/precache-manifest.e7394bde5fbdab02e02654250d7d1b35.js"
+  "/tedzvidba.github.io/precache-manifest.b580d10279016b57a6ce044c23fd29bc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tedzvidba.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
